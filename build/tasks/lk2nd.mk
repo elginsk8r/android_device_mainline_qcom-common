7cb59f39ab573bae51413310a104aff70b2b32cb
Nick Reuter <nreuter85@gmail.com>
2026-02-08 15:25:58 -0600

    qcom-common: Cleanup lk2nd build task
    Change-Id: Ia0eb98d1b31092d1e5793fe29f9b929cbadfe031

--- build/tasks/lk2nd.mk
+++ build/tasks/lk2nd.mk
@@ -9,20 +9,27 @@ ifneq ($(TARGET_LK2ND_PLATFORM),)
 
 LK2ND_MAKE_CMD := $(BUILD_TOP)/prebuilts/build-tools/$(HOST_PREBUILT_TAG)/bin/make
 LK2ND_OUT_DIR := $(TARGET_OUT_INTERMEDIATES)/LK2ND_OBJ
-LK2ND_PATH_OVERRIDE := PATH=$(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin:$(BUILD_TOP)/prebuilts/kernel-build-tools/linux-x86/bin:$$PATH
+LK2ND_PATH_OVERRIDE :=  \
+    $(BUILD_TOP)/prebuilts/tools-lineage/$(HOST_PREBUILT_TAG)/bin \
+    $(BUILD_TOP)/prebuilts/kernel-build-tools/$(HOST_PREBUILT_TAG)/bin \
+    $(BUILD_TOP)/prebuilts/gcc/$(HOST_PREBUILT_TAG)/arm/arm-none-eabi-10.2/bin
+LK2ND_PATH_OVERRIDE := $(subst $(space),:,$(strip $(LK2ND_PATH_OVERRIDE)))
+LK2ND_TOOLCHAIN_PREFIX := arm-none-eabi-
 
-LK2ND_MAKE_FLAGS := \
-    -C $(LK2ND_SOURCE) \
-    -j$(shell getconf _NPROCESSORS_ONLN) \
-    BOOTLOADER_OUT=$(BUILD_TOP)/$(LK2ND_OUT_DIR) \
-    DTC=$(BUILD_TOP)/$(HOST_OUT_EXECUTABLES)/dtc \
-    TOOLCHAIN_PREFIX=$(BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- \
-    $(TARGET_LK2ND_MAKE_FLAGS)
+define make-lk2nd-target
+@mkdir -p $(LK2ND_OUT_DIR)
+PATH=$(LK2ND_PATH_OVERRIDE):$$PATH \
+	$(LK2ND_MAKE_CMD) \
+	-C $(LK2ND_SOURCE) \
+	BOOTLOADER_OUT=$(BUILD_TOP)/$(1) \
+	TOOLCHAIN_PREFIX=$(LK2ND_TOOLCHAIN_PREFIX) \
+	$(TARGET_LK2ND_MAKE_FLAGS) \
+	$(2)
+endef
 
 $(INSTALLED_LK2NDIMAGE_TARGET): $(HOST_OUT_EXECUTABLES)/dtc
 	$(call pretty,"Target lk2nd image: $@")
-	mkdir -p $(LK2ND_OUT_DIR)
-	$(LK2ND_PATH_OVERRIDE) $(LK2ND_MAKE_CMD) $(strip $(LK2ND_MAKE_FLAGS)) lk2nd-$(TARGET_LK2ND_PLATFORM)
+	$(hide) $(call make-lk2nd-target,$(LK2ND_OUT_DIR),lk2nd-$(TARGET_LK2ND_PLATFORM))
 	cp $(LK2ND_OUT_DIR)/build-lk2nd-$(TARGET_LK2ND_PLATFORM)/lk2nd.img $@
 
 .PHONY: lk2nd
